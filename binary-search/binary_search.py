6f4bc1ba5a159f7f1a35134111bbb28e498ee863
Vyctoria Karina <vyctoriak@gmail.com>
2023-03-24 19:54:24 +0100

    binary search function in python

---
+++ binary-search/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(array, item, begin=0, end=None):
+    if end is None:
+        end = len(array)-1
+    if begin <= end:
+        m = (begin + end)//2
+        if array[m] == item:
+            return m
+        if item < array[m]:
+            return binary_search(array, item, begin, m-1)
+        else:
+            return binary_search(array, item, m+1, end)
+    return None
+
+
+lista = [2, 3, 4]
+binary_search(lista, 4)
